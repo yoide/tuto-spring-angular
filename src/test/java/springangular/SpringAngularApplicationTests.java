7f8b484899655b2f6a7a5104547a8739ec0e0e07
yoidel <yoidel94@gmail.com>
2020-07-06 19:10:18 +0200

    make it better

---
+++ src/test/java/springangular/SpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package springangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
